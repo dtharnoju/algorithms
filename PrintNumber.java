0309d4f91442be880f9977db21752af2162c14ef
dtharnoju <tharnojud@gmail.com>
2025-09-26 19:32:00 +0100

    updated file for printnumber

--- PrintNumber.java
+++ PrintNumber.java
@@ -1,12 +1,25 @@
+
+import java.io.IOException;
+import java.util.Scanner;
+
+
 public class PrintNumber{
-    public static void main(String[] args) {
+
+    public static int DEFAULT_CODE = 10001;
+
+    public static int add(int a, int b){
+        return a+b;
+    }
+
+    public static void main(String[] args) throws IOException {
         int myNum=0;
-        System.out.print("Enter a number: ");
-        try {
-            myNum=System.in.read();
-        } catch (Exception e) {
-            e.printStackTrace();
+        try (Scanner scanner = new Scanner(System.in)) {
+            System.out.print("Enter a Number:");
+            myNum=scanner.nextInt();
         }
-        System.out.println("My Number Is "+myNum);
+        
+        System.out.println("You have entered: "+myNum + " " + DEFAULT_CODE);
+
+        System.out.println("Add Number : "+add(10,20));
     }
 }
